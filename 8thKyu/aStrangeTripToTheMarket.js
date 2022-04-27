6dcd56b9d35b978dccea53282097b5a644564bbc
rakicodes <goraquel22@gmail.com>
2022-04-27 09:50:26 -0600

    add daily coding challenge

---
+++ 8thKyu/aStrangeTripToTheMarket.js
@@ -0,0 +1,4 @@
+function isLockNessMonster(s) {
+    console.log()
+    return s.toLowerCase().includes('tree fiddy') || s.toLowerCase().includes('three fifty') || s.toLowerCase().includes('3.50');
+  }
