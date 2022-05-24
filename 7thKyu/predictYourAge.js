f062c831b8b85c90f0a8a911978feead73f674dd
rakicodes <goraquel22@gmail.com>
2022-05-24 10:33:56 -0600

    add daily coding challenge

---
+++ 7thKyu/predictYourAge.js
@@ -0,0 +1,4 @@
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+    let ages = [age1,age2,age3,age4,age5,age6,age7,age8];
+    return Math.floor(Math.sqrt(ages.map(age => age*age).reduce((acc,c) => acc+c, 0))/2)
+  }
